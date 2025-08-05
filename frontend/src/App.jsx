ffd5246cab9476490ab41885d826c15cb975c5bf
orbas1 <ironcrest96@gmail.com>
2025-08-06 00:31:04 +0100

    feat: add connection management page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ChakraProvider, Box } from '@chakra-ui/react';
+import NavBar from './components/NavBar.jsx';
 import { AuthProvider, useAuth } from './context/AuthContext.jsx';
+import { ProfileProvider } from './context/ProfileContext.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import SignupPage from './pages/SignupPage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
+import ProfileCustomizationPage from './pages/ProfileCustomizationPage.jsx';
+import OrderManagementPage from './pages/OrderManagementPage.jsx';
+import ConnectionManagementPage from './pages/ConnectionManagementPage.jsx';
 
 function Protected({ children }) {
   const { user } = useAuth();
@@ -15,48 +21,53 @@ function Protected({ children }) {
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route
-            path="/profile"
-            element={
-              <Protected>
-                <ProfilePage />
-              </Protected>
-            }
-          />
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  );
-}
-import { ChakraProvider, Box } from '@chakra-ui/react';
-import NavBar from './components/NavBar.jsx';
-import ProfilePage from './pages/ProfilePage.jsx';
-import ProfileCustomizationPage from './pages/ProfileCustomizationPage.jsx';
-import { ProfileProvider } from './context/ProfileContext.jsx';
-
-function App() {
-  return (
     <ChakraProvider>
-      <BrowserRouter>
+      <AuthProvider>
         <ProfileProvider>
-          <NavBar />
-          <Box p={4}>
-            <Routes>
-              <Route path="/" element={<Navigate to="/profile" replace />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/profile/customize" element={<ProfileCustomizationPage />} />
-            </Routes>
-          </Box>
+          <BrowserRouter>
+            <NavBar />
+            <Box p={4}>
+              <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route
+                  path="/profile"
+                  element={
+                    <Protected>
+                      <ProfilePage />
+                    </Protected>
+                  }
+                />
+                <Route
+                  path="/profile/customize"
+                  element={
+                    <Protected>
+                      <ProfileCustomizationPage />
+                    </Protected>
+                  }
+                />
+                <Route
+                  path="/connections"
+                  element={
+                    <Protected>
+                      <ConnectionManagementPage />
+                    </Protected>
+                  }
+                />
+                <Route
+                  path="/orders"
+                  element={
+                    <Protected>
+                      <OrderManagementPage />
+                    </Protected>
+                  }
+                />
+                <Route path="*" element={<Navigate to="/profile" replace />} />
+              </Routes>
+            </Box>
+          </BrowserRouter>
         </ProfileProvider>
-      </BrowserRouter>
+      </AuthProvider>
     </ChakraProvider>
   );
 }
-
-export default App;
