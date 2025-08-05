b81811d3544df78c3ea4f543b0308139ccffee92
orbas1 <ironcrest96@gmail.com>
2025-08-05 20:13:28 +0100

    feat: add learning path module

--- backend/app.js
+++ backend/app.js
@@ -49,6 +49,7 @@ const milestoneRoutes = require('./routes/milestones');
 const progressRoutes = require('./routes/progress');
 const achievementRoutes = require('./routes/achievements');
 const skillRoutes = require('./routes/skills');
+const learningPathRoutes = require('./routes/paths');
 
 const app = express();
 app.use(cors());
@@ -103,6 +104,7 @@ app.use('/communication', communicationRoutes);
 app.use('/progress', progressRoutes);
 app.use('/achievements', achievementRoutes);
 app.use('/skills', skillRoutes);
+app.use('/paths', learningPathRoutes);
 app.use('/goals', goalRoutes);
 app.use('/milestones', milestoneRoutes);
 
