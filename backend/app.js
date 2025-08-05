3877d9ce9ac83d1793431552741ddffb0cf34b1d
orbas1 <ironcrest96@gmail.com>
2025-08-05 20:14:10 +0100

    feat: add project module

--- backend/app.js
+++ backend/app.js
@@ -49,6 +49,7 @@ const milestoneRoutes = require('./routes/milestones');
 const progressRoutes = require('./routes/progress');
 const achievementRoutes = require('./routes/achievements');
 const skillRoutes = require('./routes/skills');
+const projectRoutes = require('./routes/projects');
 
 const app = express();
 app.use(cors());
@@ -103,6 +104,7 @@ app.use('/communication', communicationRoutes);
 app.use('/progress', progressRoutes);
 app.use('/achievements', achievementRoutes);
 app.use('/skills', skillRoutes);
+app.use('/projects', projectRoutes);
 app.use('/goals', goalRoutes);
 app.use('/milestones', milestoneRoutes);
 
